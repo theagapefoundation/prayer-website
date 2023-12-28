d656ea37e13d35103a445c167f5c8d4d3ebb0d32
Jay Cho <jaycho1214@gmail.com>
2023-12-28 23:19:58 +0900

    set status code to 200

--- website/views.py
+++ website/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.template import RequestContext
 
 
 def handler404(request, *args, **argv):
     response = render(request, template_name="error_page.html")
-    response.status_code = 404
+    response.status_code = 200
     return response
 
 
